4e3f0c46791f8b3da03ddc1c19cc40b9b95f08b0
richcode <richardchanme@gmail.com>
2017-03-07 07:43:06 -0800

    updating data

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -47,9 +47,6 @@ var bio = {
 
 };
 
-bio.display(); // DISPLAYING ALL BIO ON PAGE
-
-
 // EDUCATION SECTION INCLUDING DISPLAY FUNCTION
 
 var education = {
@@ -73,7 +70,7 @@ var education = {
     "onlineCourses": [{
         "title": "Front-End Web Development",
         "school": "Udacity",
-        "dates": 2017,
+        "dates": "2017",
         "url": "https://www.udalocation.com/course/front-end-web-developer-nanodegree--nd001",
     }],
     "display": function() {
@@ -122,8 +119,6 @@ var education = {
         });
     }
 };
-
-education.display(); // DISPLAYING EDUCATION ON PAGE
 
 
 // WORK SECTION INCLUDING DISPLAY FUNCTION
@@ -196,55 +191,53 @@ var work = {
 
 };
 
-work.display(); // DISPLAYING WORK ON PAGE
-
 
 // PROJECTS SECTION INCLUDING DISPLAY FUNCTION
 
-var projects = {
+var project = {
     "projects": [{
             "title": "Classic Arcade Game",
-            "dates": 2017,
+            "dates": "2017",
             "description": "You will be provided with visual assets and a game loop engine; using these tools you must add a number of entities to the game including the player characters and enemies to recreate the classic arcade game Frogger.",
             "images": ["http://assets.barcroftmedia.com.s3-website-eu-west-1.amazonaws.com/assets/images/recent-images-11.jpg", ],
         },
         {
             "title": "Website Optimization",
-            "dates": 2017,
+            "dates": "2017",
             "description": "You will optimize a provided website with a number of optimization- and performance-related issues so that it achieves a target PageSpeed score and runs at 60 frames per second.",
             "images": ["http://assets.barcroftmedia.com.s3-website-eu-west-1.amazonaws.com/assets/images/recent-images-11.jpg", ],
         },
         {
             "title": "Neighborhood Map",
-            "dates": 2017,
+            "dates": "2017",
             "description": "You will develop a single-page application featuring a map of your neighborhood or a neighborhood you would like to visit. You will then add additional functionality to this application, including: map markers to identify popular locations or places you’d like to visit, a search function to easily discover these locations, and a listview to support simple browsing of all locations. You will then research and implement third-party APIs that provide additional information about each of these locations (such as StreetView images, Wikipedia articles, Yelp reviews, etc).",
             "images": ["http://assets.barcroftmedia.com.s3-website-eu-west-1.amazonaws.com/assets/images/recent-images-11.jpg", ],
         },
         {
             "title": "Feed Reader Testing",
-            "dates": 2017,
+            "dates": "2017",
             "description": "In this project, you will be learning about testing with Javascript. Testing is an important part of the development process and many organizations practice a standard known as 'test-driven development' or TDD. This is when developers write tests first, before they ever start developing their application. Whether you work in an organization that writes tests extensively to inform product development or one that uses tests to encourage iteration, testing has become an essential skill in modern web development!",
             "images": ["http://assets.barcroftmedia.com.s3-website-eu-west-1.amazonaws.com/assets/images/recent-images-11.jpg", ],
         }],
     "display": function() {
 
-        $.each(projects.projects, function(proj) {
+        $.each(project.projects, function(proj) {
 
             $('#projects').append(HTMLprojectStart); // this appends PROJECTS to #projects header
 
-            var projectss = projects.projects[proj];
+            var projectsss = project.projects[proj];
 
-            var myTitle = HTMLprojectTitle.replace('%data%', projectss.title);
+            var myTitle = HTMLprojectTitle.replace('%data%', projectsss.title);
             $('.project-entry:last').append(myTitle);
 
-            var myDates = HTMLprojectDates.replace('%data%', projectss.dates);
+            var myDates = HTMLprojectDates.replace('%data%', projectsss.dates);
             $('.project-entry:last').append(myDates);
 
-            var myDescription = HTMLprojectDescription.replace('%data%', projectss.description);
+            var myDescription = HTMLprojectDescription.replace('%data%', projectsss.description);
             $('.project-entry:last').append(myDescription);
 
-            $.each(projectss.images, function(img) {
-                var myImages = HTMLprojectImage.replace('%data%', projectss.images[img]);
+            $.each(projectsss.images, function(img) {
+                var myImages = HTMLprojectImage.replace('%data%', projectsss.images[img]);
                 $('.project-entry:last').append(myImages);
             });
 
@@ -254,7 +247,10 @@ var projects = {
 
 };
 
-projects.display(); // DISPLAYING PROJECTS ON PAGE
+bio.display(); // DISPLAYING ALL BIO ON PAGE
+education.display(); // DISPLAYING EDUCATION ON PAGE
+work.display(); // DISPLAYING WORK ON PAGE
+project.display(); // DISPLAYING PROJECTS ON PAGE
 
 
 // SHOWING MAP AT LAST
