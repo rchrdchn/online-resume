901578f56b45d9bfd3ac5fc07b26c89b3048e5b4
Richard Chan <richardchanme@gmail.com>
2017-07-11 00:03:35 -0700

    working on adding helper and url to contact info

--- resume/js/helper.js
+++ resume/js/helper.js
@@ -22,6 +22,7 @@ var HTMLtwitter = '<li class="flex-item"><span class="orange-text">twitter</span
 var HTMLgithub = '<li class="flex-item"><span class="orange-text">github</span><span class="white-text">%data%</span></li>';
 var HTMLblog = '<li class="flex-item"><span class="orange-text">blog</span><span class="white-text">%data%</span></li>';
 var HTMLlocation = '<li class="flex-item"><span class="orange-text">location</span><span class="white-text">%data%</span></li>';
+var HTMLurl = '<a href=#>%data%';
 
 var HTMLbioPic = '<img src="%data%" class="biopic">';
 var HTMLwelcomeMsg = '<span class="welcome-message">%data%</span>';
